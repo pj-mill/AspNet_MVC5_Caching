a4b52daff7fc408deea4f975eb1763de6c7d88ea
Paul <paul.j.millar.1971@gmail.com>
2016-10-23 12:11:58 +0100

    Initial Commit

---
+++ OutputCacheExample/Controllers/CachingController.cs
@@ -0,0 +1,16 @@
+﻿using OutputCacheExample.Models;
+using System;
+using System.Web.Mvc;
+
+namespace OutputCacheExample.Controllers
+{
+    public class CachingController : Controller
+    {
+        [OutputCache(Duration = 10)]
+        public ViewResult Index()
+        {
+            Person person = new Person() { Name = "John", Created = DateTime.Now };
+            return View(person);
+        }
+    }
+}
---
+++ OutputCacheExample/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OutputCacheExample.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
