ec6b768a3e42a93ff8b9fa9c1701f4dd3fc81cf8
Paul <paul.j.millar.1971@gmail.com>
2016-10-23 13:13:57 +0100

    Added Cache Profiles

--- OutputCacheExample/Controllers/CachingController.cs
+++ OutputCacheExample/Controllers/CachingController.cs
@@ -9,7 +9,23 @@ namespace OutputCacheExample.Controllers
         [OutputCache(Duration = 10)]
         public ViewResult Index()
         {
+            // A new person object will only be passed to the view every 10 seconds.
+            // Otherwise, it will be retrieved from the cache.
             Person person = new Person() { Name = "John", Created = DateTime.Now };
+            return View(person);
+        }
+
+        [OutputCache(CacheProfile = "Aggressive")]
+        public ViewResult AggressiveCacheProfile()
+        {
+            Person person = new Person() { Name = "Aggressive Profile", Created = DateTime.Now };
+            return View(person);
+        }
+
+        [OutputCache(CacheProfile = "Short")]
+        public ViewResult ShortCacheProfile()
+        {
+            Person person = new Person() { Name = "Short Profile", Created = DateTime.Now };
             return View(person);
         }
     }
